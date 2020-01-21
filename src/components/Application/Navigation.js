7f0f015ef8333a0760c4e352d084e1e8ae0fd65b
Devin Black <Devinjamesblack@gmail.com>
2020-01-21 07:35:42 -0700

    adding features to nav

--- src/components/Application/Navigation.js
+++ src/components/Application/Navigation.js
@@ -10,11 +10,11 @@ const Navigation = () => {
 
       <nav className="navigation__nav">
         <ul className="navigation__list">
-          <li className="navigation__item"><a href="localhost:3000" className="navigation__link">About Natours</a></li>
-          <li className="navigation__item"><a href="localhost:3000" className="navigation__link">Your Benefits</a></li>
-          <li className="navigation__item"><a href="localhost:3000" className="navigation__link">Popular Tours</a></li>
-          <li className="navigation__item"><a href="localhost:3000" className="navigation__link">Stories</a></li>
-          <li className="navigation__item"><a href="localhost:3000" className="navigation__link">Book Now</a></li>
+          <li className="navigation__item"><a href="localhost:3000" className="navigation__link"><span>01</span>About Natours</a></li>
+          <li className="navigation__item"><a href="localhost:3000" className="navigation__link"><span>02</span>Your Benefits</a></li>
+          <li className="navigation__item"><a href="localhost:3000" className="navigation__link"><span>03</span>Popular Tours</a></li>
+          <li className="navigation__item"><a href="localhost:3000" className="navigation__link"><span>04</span>Stories</a></li>
+          <li className="navigation__item"><a href="localhost:3000" className="navigation__link"><span>05</span>Book Now</a></li>
         </ul>
       </nav>
     </div>
